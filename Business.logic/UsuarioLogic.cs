6dd4ce5f9d447be9c6ee65260eb6a24a86b4458a
Manuel Bahamonde <manuelbahamondedev@gmail.com>
2020-05-01 20:05:56 -0300

    Agrego la parte 4 (modifique el anterior commit para que funcione con lo que agregue)

---
+++ Business.logic/UsuarioLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Entities;
+using Data.Database;
+namespace Business.logic {
+    public class UsuarioLogic:BusinessLogic {
+        UsuarioAdapter usuarioData;
+
+        public UsuarioLogic() {
+            usuarioData = new UsuarioAdapter();
+        }
+
+        UsuarioAdapter UsuarioData {
+            get { return usuarioData; }
+            set { usuarioData = value; }
+        }
+
+        public Usuario getOne(int id) {
+            return usuarioData.GetOne(id);
+        }
+        public List<Usuario> getAll() {
+            return usuarioData.GetAll();
+        }
+        public void save(Usuario user) {
+            usuarioData.Save(user);
+        }
+        public void delete(int id) {
+            usuarioData.Delete(id);
+        }
+    }
+}
