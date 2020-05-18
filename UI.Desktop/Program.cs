dd0e21205a26eece0fa67aa96a6f2952c79aef59
Manuel Bahamonde <manuelbahamondedev@gmail.com>
2020-05-18 17:42:04 -0300

    das

--- UI.Desktop/Program.cs
+++ UI.Desktop/Program.cs
@@ -14,6 +14,7 @@ namespace UI.Desktop
         [STAThread]
         static void Main()
         {
+            //prueba
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
